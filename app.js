a895d82b82640982db84b336393ff397e7abb3f4
Thanakorn <em_thanakorn@Thanakorns-MacBook-Air.local>
2024-11-12 12:10:09 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+let [seconds, minutes, hours] = [0, 0, 0];
+let displayTime = document.getElementById("displayTime");
+let timer = null;
+
+function stopWatch() {
+  seconds++;
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  let hh = hours < 10 ? "0" + hours : hours;
+  let mm = minutes < 10 ? "0" + minutes : minutes;
+  let ss = seconds < 10 ? "0" + seconds : seconds;
+
+  displayTime.innerHTML = hh + ":" + mm + ":" + ss;
+}
+
+function watchStart() {
+  if (timer !== null) {
+    clearInterval(timer);
+  }
+  timer = setInterval(stopWatch, 1);
+}
+
+function watchStop() {
+  clearInterval(timer);
+}
+
+function watchReset() {
+  clearInterval(timer);
+  // [seconds, minutes, hours] = [0, 0, 0];
+  displayTime.innerHTML = "00:00:00";
+}
